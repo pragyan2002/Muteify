b8b17fb238dc2916e8a758a2c11698125398bc78
pragyan2002 <pragyanshankar.shukla2020@vitstudent.ac.in>
2024-12-29 16:14:47 -0500

    is_spotify_running

--- muteify.py
+++ muteify.py
@@ -2,8 +2,15 @@ import psutil
 import time
 import requests
 
-def is_spotify_running():
-    return
+def is_spotify_running() -> bool:
+    """
+    Check if Spotify.exe is running.
+    Returns True if found, otherwise False.
+    """
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] and proc.info['name'].lower() == "spotify.exe":
+            return True
+    return False
 
 def get_spotify_metadata():
     return
@@ -14,3 +21,6 @@ def get_current_volume():
 def set_spotify_volume():
     return
 
+if __name__ == "__main__":
+    print("Test is_spotify_running()...")
+    print("Is Spotify running: ", is_spotify_running())
