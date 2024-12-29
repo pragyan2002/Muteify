e8b5f427bb7910b17f25bad88fd0dd67d53fded5
pragyan2002 <pragyanshankar.shukla2020@vitstudent.ac.in>
2024-12-29 16:09:24 -0500

    Skeleton

---
+++ muteify.py
@@ -0,0 +1,16 @@
+import psutil
+import time
+import requests
+
+def is_spotify_running():
+    return
+
+def get_spotify_metadata():
+    return
+
+def get_current_volume():
+    return 
+
+def set_spotify_volume():
+    return
+
